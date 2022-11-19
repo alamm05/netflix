131850b1d05e73fa358a8d3f08afa9a9d486521e
Rashid Alam <alamm2412@gmail.com>
2022-11-19 14:33:22 +0530

    :completed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Home.scss"
-import axios from ''
+import axios from "axios";
+import {BiPlay} from "react-icons/bi"
+import {AiOutlinePlus} from "react-icons/ai"
+
+
+
+
+const apiKey = "df993b768d2ad429b2c8a94fad44816a";
+const url = "https://api.themoviedb.org/3";
+const imgUrl ="https://image.tmdb.org/t/p/original"
+const upcoming = "upcoming";
+const nowPlaying = "now_playing";
+const popular = "popular";
+const topRated = "top_rated";
 
 const Card = ({img}) =>(
   <img className='card' src={img} alt="cover" />
 )
-const Row = ({title,arr=[{
-img:"https://static1.colliderimages.com/wordpress/wp-content/uploads/avengers-character-poster-banner-slice.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
-}]}) =>(
+const Row = ({title,
+  arr = [
+  {
+img:""
+},
+],
+}) =>(
   <div className='row'>
     <h2>{title}</h2>
     <div>
       {
-        arr.map((item) =>(
-          <Card img={item.img}/>
+        arr.map((item,index) =>(
+          <Card key = {index} img={`${imgUrl}/${item.poster_path}`}/>
         ))
       }
     
@@ -23,13 +40,59 @@ img:"https://static1.colliderimages.com/wordpress/wp-content/uploads/avengers-ch
 )
 
 const Home = () => {
+  const [upcomingMovies,setUpcomingMovies] =useState([])
+  const [nowPlayingMovies,setNowPlayingMovies] =useState([])
+  const [popularMovies,setPopularMovies] =useState([])
+  const [topRatedMovies,setTopRatedMovies] =useState([])
+  const [genre,setGenre] =useState([])
+  useEffect(()=> {
+    const fetchUpcoming = async()=>{
+      const {data : {results}} = await axios.get(`${url}/movie/${upcoming}?api_key=${apiKey}`)
+      setUpcomingMovies(results);
+    };
+
+    const fetchNowPlaying = async()=>{
+      const {data : {results}} = await axios.get(`${url}/movie/${nowPlaying}?api_key=${apiKey}`)
+      setNowPlayingMovies(results);
+    };
+    const fetchPopular = async()=>{
+      const {data : {results}} = await axios.get(`${url}/movie/${popular}?api_key=${apiKey}`)
+      setPopularMovies(results);
+    };
+    const fetchTopRated = async()=>{
+      const {data : {results}} = await axios.get(`${url}/movie/${topRated}?api_key=${apiKey}`)
+      setTopRatedMovies(results);
+    };
+    const getAllGenre = async()=>{
+      const {data : {results}} = await axios.get(`${url}/genre/movie/list?api_key=${apiKey}`)
+      setGenre(results);
+    };
+    getAllGenre()
+    fetchUpcoming()
+    fetchNowPlaying()
+    fetchPopular()
+    fetchTopRated()
+  },[])
   return (
     <section className='home'>
-      <div className='banner'></div>
-    
-      <Row  title={"Popular on Netflix"}/>
-      <Row  title={"Popular on Netflix"}/>
-      <Row  title={"Popular on Netflix"}/>
+      <div className='banner' style={{
+        backgroundImage:popularMovies[0]? `url(${`${imgUrl}/${popularMovies[0].poster_path}`})`:"rgb(16,16,16)",
+      }}>
+
+        {popularMovies[0] && <h1>{popularMovies[0].original_title}</h1>}
+
+        {popularMovies[0] && <p>{popularMovies[0].overview}</p>}
+        <button> <BiPlay />Play </button>
+        <button>My List <AiOutlinePlus /></button>
+       
+      </div>
+     
+      <Row  title={"Upcoming movies"} arr ={upcomingMovies}/>
+      <Row  title={"Now palying m"} arr ={nowPlayingMovies}/>
+      <Row  title={"Popular "} arr ={popularMovies}/>
+      <Row  title={"Top Rated "} arr ={topRatedMovies}/>
+      
+      
      
     </section>
   )
